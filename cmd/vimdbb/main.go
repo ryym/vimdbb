bed195de13e7c9beddfe7f1db4aa25c92ce520f6
ryym <ryym.64@gmail.com>
2017-12-03 09:37:35 +0900

    fix: Change default port number

--- cmd/vimdbb/main.go
+++ cmd/vimdbb/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/ryym/vimdbb/server"
 	"net"
 	"os"
+
+	"github.com/ryym/vimdbb/server"
 )
 
 func main() {
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", ":4242")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
